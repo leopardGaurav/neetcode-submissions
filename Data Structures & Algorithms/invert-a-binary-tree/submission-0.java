6aa5e45a136397ca478985101ee76db99566c6df
Gaurav kumar <165229582+leopardGaurav@users.noreply.github.com>
2026-08-16 18:05:45 +0530

    Add: invert-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-0.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+
+        // Swap the left and right children
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        // Recursively invert both subtrees
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
