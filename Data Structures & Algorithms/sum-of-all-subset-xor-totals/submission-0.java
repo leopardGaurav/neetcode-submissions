7a01ef614425696b6918a401946a02fad8596abe
Gaurav kumar <165229582+leopardGaurav@users.noreply.github.com>
2026-09-04 09:33:29 +0530

    Add: sum-of-all-subset-xor-totals - submission-0

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        return dfs(nums, 0, 0);
+    }
+
+    private int dfs(int[] nums, int index, int currentXOR) {
+        if (index == nums.length) {
+            return currentXOR;
+        }
+
+        // Include current number XOR Exclude current number
+        int include = dfs(nums, index + 1, currentXOR ^ nums[index]);
+        int exclude = dfs(nums, index + 1, currentXOR);
+
+        return include + exclude;
+    }
+}
